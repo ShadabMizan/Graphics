c2f55fa72e36f636593c96930e89c3856e953254
Shadab Mizan <shadabsmizan@gmail.com>
2024-07-08 15:00:35 -0700

    Changed a folder name

---
+++ Blocks/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Camera.h"
+
+int main(int argc, char const *argv[])
+{
+    Camera camera(50, 35, 24, 0.1, 100, Vec3f(0.5, -9, 3.5), Vec3f(77, 0, 5));
+
+    
+    return 0;
+}
